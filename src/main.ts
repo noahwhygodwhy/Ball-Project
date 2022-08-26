19cdecf44c9eec34f434312c4a949c7a5ee808b1
noahwhygodwhy <noah.mathnerd.alvard@gmail.com>
2022-08-25 21:47:21 -0400

    basic project initialized

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+
+
+
+function main() {
+    const canvas = document.querySelector("#glCanvas") as HTMLCanvasElement;
+    // Initialize the GL context
+    if(canvas == null) {
+        console.log("Canvas is null")
+        return
+    }
+    const gl = canvas.getContext("webgl2");
+  
+    // Only continue if WebGL is available and working
+    if (gl === null) {
+      alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+      return;
+    }
+  
+    
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+  }
+  
+  window.onload = main;
