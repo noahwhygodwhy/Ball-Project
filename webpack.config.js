dd8375c178eeb06703b4e4bc68b48f38cee4a730
noahwhygodwhy <noah.mathnerd.alvard@gmail.com>
2022-08-27 09:13:57 -0400

    got webpack working :O

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './build/graphics.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: "production",
+    entry: './build/graphics.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
