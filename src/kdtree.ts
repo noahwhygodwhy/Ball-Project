e407f238d04195740cae15064c7557c92409d879
noahwhygodwhy <noah.mathnerd.alvard@gmail.com>
2022-09-01 16:09:30 -0400

    continued kdnode class

--- src/kdtree.ts
+++ src/kdtree.ts
@@ -1,14 +1,19 @@
 import * as glm from "gl-matrix" 
 
 class kdNode {
+    value:number
+    axis:number
     balls:Array<number>
-    constructor(balls:Array<number>) {
-        this.balls = balls;
-    }
-}
-class kdBranch extends kdNode {
-    
-}
-class kdLeaf extends kdNode {
+    lesserChild:kdNode
+    greaterChild:kdNode
+    constructor(balls:Array<Array<number>>, theList:Array<number>, layer:number) {
+        this.axis = layer%3
+        theList.sort((a:number, b:number)=> (balls[a][this.axis]-balls[b][this.axis]))
 
+        let middleIdx:number = theList.length/2
+        let middleValue = balls[middleIdx][this.axis]
+
+        this.balls = balls;
+
+    }
 }
